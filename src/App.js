df44ea58f7d611e54a8ccbcbeb52d8d7098c379a
mvds-io <240583@gmail.com>
2024-09-25 22:03:42 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Dette FUNKER! IGJEN!!!
+        Dette FUNKER! IGJEN!!! Og igjen og igjen og <B>igjen.</B>
       </header>
     </div>
   );
